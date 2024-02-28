ab9212cac69c61ba0c1f21e7bcca174e0a71fd5f
ReV2007 <145356412+ReV2007@users.noreply.github.com>
2024-02-28 09:50:11 -0500

    Finished exercise

---
+++ exercise8/exercise8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pick a number, any number ,':) ");
+            int numberpicked = int.Parse(Console.ReadLine());
+
+            for ( int evennumbers = 0; evennumbers <= numberpicked; evennumbers++ , evennumbers++)
+            {
+                Console.WriteLine(evennumbers);
+            }
+
+        }
+    }
+}
